f71ba7a02b5fe80a432449fdc174e00914644912
Hush <invalid@live.cn>
2022-06-20 18:08:50 +0800

    Fix:fix razord

--- app/src/main/java/com/hush/yacd/data/FrontStyle.kt
+++ app/src/main/java/com/hush/yacd/data/FrontStyle.kt
@@ -2,7 +2,7 @@ package com.hush.yacd.data
 
 enum class FrontStyle(val url: String) {
     YACD("file:///android_asset/yacd/index.html?hostname=%s&port=%s&secret=%s#/proxies"),
-    RAZORD("https://clash.razord.top/#/proxies?host=%s&port=%s&secret=%s"),
+    RAZORD("http://clash.razord.top/#/proxies?host=%s&port=%s&secret=%s"),
 }
 
 fun FrontStyle.configUrl(hostName: String, port: String, secret: String): String {
